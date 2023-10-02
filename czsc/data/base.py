466a79373a0b8d6a69e4f632dc2a2f8ff883c772
Robin202201 <97595032+Robin202201@users.noreply.github.com>
2023-10-02 20:23:32 +0800

    变更

--- czsc/data/base.py
+++ czsc/data/base.py
@@ -5,9 +5,16 @@ email: zeng_bin8888@163.com
 create_dt: 2022/2/24 16:17
 describe: 基础工具
 """
+#该代码实现的功能是将中文频率转换为英文频率。具体步骤如下：
+
+#1. 创建一个名为`freq_cn2ts`的字典。
+#2. 字典中的键是中文频率，值是对应的英文频率。
+#3. 中文频率包括："1分钟"、"5分钟"、"15分钟"、"30分钟"、"60分钟"、"日线"、"周线"、"月线"。
+#4. 对应的英文频率分别是："1min"、"5min"、"15min"、"30min"、"60min"、"D"、"W"、"M"。
 
 freq_cn2ts = {"1分钟": "1min", "5分钟": "5min", "15分钟": "15min", "30分钟": "30min",
               '60分钟': "60min", "日线": "D", "周线": "W", "月线": "M"}
+#该代码实现的功能是将一个字典中的键和值进行交换，并创建一个新的字典。
 freq_ts2cn = {v: k for k, v in freq_cn2ts.items()}
 
 freq_cn2jq = freq_cn2ts
@@ -134,14 +141,18 @@ def ts_symbol_to_tdx(symbol):
 
 
 def save_symbols_to_ebk(symbols, file_ebk, source='ts'):
-    """将股票代码列表保存到 EBK 文件，用来导入标的到同花顺、通达信软件中
+    """
+    将股票代码列表保存到 EBK 文件，用来导入标的到同花顺、通达信软件中
 
     :param symbols: 股票代码列表
     :param file_ebk: EBK结果文件
-    :param source: 代码格式
-    :return:
+    :param source: 代码格式，可选值为 'ts'、'jq' 或 'gm'
+    :return: 无返回值
     """
+    # 将source参数转换为小写
     source = source.lower()
+
+    # 根据source参数选择符号映射函数
     if source == 'ts':
         symbol_to_tdx = ts_symbol_to_tdx
     elif source == 'jq':
@@ -149,9 +160,13 @@ def save_symbols_to_ebk(symbols, file_ebk, source='ts'):
     elif source == 'gm':
         symbol_to_tdx = gm_symbol_to_tdx
     else:
+        # 如果source参数不在可选值中，引发ValueError异常
         raise ValueError
 
+    # 使用符号映射函数将股票代码列表转换为通达信格式的代码列表
     tdx_symbols = [symbol_to_tdx(ts_code) for ts_code in symbols]
+
+    # 打开EBK结果文件，并将转换后的代码列表写入文件中
     with open(file_ebk, encoding='utf-8', mode='w') as f:
         f.write("\n".join(tdx_symbols))
 
